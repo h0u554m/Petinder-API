6ecff0dc453b6301246f1ad661b2ced269d0adf8
Houssam Eddine Marrakchi <houssammarrakchi3@gmail.com>
2023-12-12 14:11:23 +0100

    Initiating Warp Drive 🚀

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project Title
+
+Brief project description and purpose.
+
+## Table of Contents
+
+- [Project Title](#project-title)
+  - [Table of Contents](#table-of-contents)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+
+## Getting Started
+
+### Prerequisites
+
+List any software, tools, or dependencies that need to be installed before running the project.
+
+### Installation
+
+1. Clone the repository.
+   ```sh
+   git clone https://github.com/yourusername/yourproject.git
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+
+// Create Express App
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up your routes
+/* const indexRoute = require('./routes/index');
+const userRoute = require('./routes/user');
+
+app.use('/', indexRoute);
+app.use('/users', userRoute); */
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
