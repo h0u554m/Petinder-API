27f817883146b37087c0f1b5d8ec469389d718da
fridavbg <fperssontech@gmail.com>
2023-12-21 14:46:08 +0100

    Swagger inital setup

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 // Create Express App
 const app = express();
@@ -15,20 +17,43 @@ app.use(morgan("dev"));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// Swagger Configuration
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "Petinder Online API",
+        },
+
+        servers: [
+            {
+                url: "http://localhost:3000",
+                description: "Pettinder API Documentation",
+            },
+        ],
+    },
+    apis: ["./Routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 // Set up your routes
 const indexRoute = require("./routes/index");
 
 app.use("/", indexRoute);
 
 app.get("/", (req, res) => {
-  res.json({
-    Api: {
-      message: "Petinder API Online",
-    },
-  });
+    res.json({
+        Api: {
+            message: "Petinder API Online",
+        },
+    });
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,71 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - title
+ *         - body
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The Auto-generated id of a post
+ *         userId:
+ *           type: integer
+ *           description: id of author
+ *         title:
+ *           type: string
+ *           description: title of post
+ *         body:
+ *           type: string
+ *           descripton: content of post *
+ *       example:
+ *         id: 1
+ *         userId: 1
+ *         title: my title
+ *         body: my article
+ *
+ */
+
+/**
+ * @swagger
+ *  /auth/login:
+ *   post:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.post("/login", authController.login);
 
+/**
+ * @swagger
+ *  /auth/register:
+ *   post:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.post("/register", authController.register);
 
 module.exports = router;
