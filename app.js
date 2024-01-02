98c60c3fb8e09cfa6754204df78f4b05a5691360
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-01-02 11:07:14 -0300

    solving erros after pull request

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
-<<<<<<< HEAD
 const resetPasswordRoute = require("./routes/resetPasswordRoute.js");
 const resetEmailRoute = require("./routes/reseteEmailRoute.js");
-=======
->>>>>>> b44c1a857267bf35ec04f2f8894a97d33335bb5a
 
 // Create Express App
 const app = express();
@@ -25,26 +22,13 @@ const indexRoute = require("./routes/index");
 
 app.use("/", indexRoute);
 
-<<<<<<< HEAD
 // route Felipe Blaksley Reset Password
 app.set("view engine", "ejs");
 app.use("/api", resetPasswordRoute);
 app.use("/api", resetEmailRoute);
 // ------------------------------------------------------------
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-=======
 app.get("/", (req, res) => {
->>>>>>> b44c1a857267bf35ec04f2f8894a97d33335bb5a
   res.json({
     Api: {
       message: "Petinder API Online",
